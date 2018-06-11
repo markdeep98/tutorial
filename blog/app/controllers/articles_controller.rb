1bb64f82e00b3d0ad19fb6c1bdf9fe2a96a093c9
markdeep98 <dmitriy.gladky98@gmail.com>
2018-06-11 05:13:07 -0700

    Test tutorial

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,56 +1,51 @@
 class ArticlesController < ApplicationController
+  before_action :article_find, only: %i[show edit update destroy]
 
-	http_basic_authenticate_with name: 'lol', password: '123456', except: [:index, :show]
+  def index
+    @articles = article_method.all
+  end
 
-	def index
-		@articles = article_method.all
-	end
-	
-	def show
-		@article = article_method.find(params[:id])
-	end
+  def show; end
 
-	def new
-		@article = article_method.new
-	end
+  def new
+    @article = article_method.new
+  end
 
-	def edit
-		@article = article_method.find(params[:id])
-	end
+  def edit; end
 
-	def create
-		@article = article_method.new(article_params)
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
-	end
+  def create
+    @article = article_method.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@article = article_method.find(params[:id])
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
-		if @article.update(article_params)
-			redirect_to @article
-		else
-			render 'edit'
-		end
-	end
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
-	def destroy
-		@article = article_method.find(params[:id])
-		@article.destroy
+  private
 
-		redirect_to articles_path
-	end
-	
-	private
+  def article_find
+    @article = article_method.find(params[:id])
+  end
 
-	def article_method
-		@art = Article
-	end
-	
-	def article_params
-		params.require(:article).permit(:title, :text)
-	end
+  def article_method
+    @art = Article
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,22 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :article_find
 
-	http_basic_authenticate_with name: 'lol', password: '123456', only: :destroy
+  def create
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
 
-	def create 
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
-		redirect_to article_path(@article)
-	end
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
 
-	def destroy
-		@article = Article.find(params[:article_id])
-		@comment = @article.comments.find(params[:id])
-		@comment.destroy
-		redirect_to article_path(@article)
-	end
-	private
+  private
 
-		def comment_params
-			params.require(:comment).permit(:commenter, :body)
-		end
+  def article_find
+    @article = Article.find(params[:article_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
